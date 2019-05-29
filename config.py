330b00a99b61fb2467e4c832245617f5eb746db1
Christian Meléndez Salinas <camelendez@uc.cl>
2019-05-29 15:41:58 -0400

    Changed images filename

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
     datasetFilename = "" # dataset specific
 
     # file names
-    imagesFilename = "{tier}.h5" # Images
+    imagesFilename = "{tier}_features.h5" # Images
     instancesFilename = "{tier}Instances.json"
     # symbols dictionaries
     questionDictFilename = "questionDict.pkl"
