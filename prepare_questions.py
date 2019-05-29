458beb1118a5ab53be8a650fe0bcd8a8bcbf122a
Geeermy <glcontreras@uc.cl>
2019-05-29 17:29:57 -0400

    Add script to prepare GQA questions

---
+++ prepare_questions.py
@@ -0,0 +1,51 @@
+'''
+Script para cambiar el formato del json de miniGQA al utilizado por la red
+MAC.
+'''
+
+# Librerías a utilizar.
+import json
+import argparse
+
+def prepare_questions(args):
+    print('Loading data...')
+    with open(args.input_questions_dir, 'r') as f:
+        data  = json.load(f)
+
+    print('Processing data...')
+    imgs_idxs = set()
+    questions = []
+    for index, question in data.items():
+        img_idx = question["imageId"]#data['image_index'][index]
+        imgs_idxs.add(img_idx)
+        q = {
+        'question': question["question"],
+        'answer': question["answer"],
+        #'program': data['program'][index], # Revisar
+        'index': int(index),
+        'image_index': img_idx,
+        'group': question["groups"],
+        'type': question["types"]
+        #'question_family_index': data['question_family_index'][index] # Revisar
+        }
+        questions.append(q)
+    #questions.sort(key=lambda x: x['index'])
+    imgs_idxs = sorted(imgs_idxs)
+    mapper = {x: i for i, x in enumerate(imgs_idxs)}
+    for q in questions:
+        q['image_index'] = mapper[q['image_index']]
+    
+    print('Saving data...')
+    with open(args.output_questions_dir, 'w') as f:
+        f.write(json.dumps(questions))
+    
+    print('Done!')
+            
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_questions_dir', required=True)
+    parser.add_argument('--output_questions_dir', required=True)
+
+    args = parser.parse_args()
+    prepare_questions(args)
